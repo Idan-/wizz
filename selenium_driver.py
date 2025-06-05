c55cbc523257fc55bb5f3ae3c8c6bbf569c593d6
idan <inahomm76@gmail.com>
2025-06-06 00:20:15 +0300

    update template and run headless

--- selenium_driver.py
+++ selenium_driver.py
@@ -18,7 +18,7 @@ def get_driver():
     # Setup Chromium options
     chrome_options = Options()
     chrome_options.add_argument('--no-sandbox')
-    # chrome_options.add_argument('--headless')
+    chrome_options.add_argument('--headless')
     chrome_options.add_argument('--disable-dev-shm-usage')
 
     # Automatically download the correct ChromeDriver version
